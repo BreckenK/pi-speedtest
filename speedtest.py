a89933c1f8736ce45b5a45a98702f6bb060595d5
Brecken Keller <bkeller@umd.edu>
2020-04-27 11:52:36 -0400

    Update speedtest.py

--- speedtest.py
+++ speedtest.py
@@ -4,12 +4,18 @@ import re
 import subprocess
 import time
 
-response = subprocess.Popen('speedtest-cli --simple', shell=True, stdout=subprocess.PIPE).stdout.read()
 
-ping = re.findall('Ping:\s(.*?)\s', response, re.MULTILINE)
-download = re.findall('Download:\s(.*?)\s', response, re.MULTILINE)
-upload = re.findall('Upload:\s(.*?)\s', response, re.MULTILINE)
 
-ping[0] = ping[0].replace(',', '.')
-download[0] = download[0].replace(',', '.')
-upload[0] = upload[0].replace(',', '.')
+if __name__ == "__main__":
+  response = subprocess.Popen('speedtest-cli --simple', shell=True, stdout=subprocess.PIPE).stdout.read()
+  blinkt.set_pixel(0, 255, 255, 255)
+  blinkt.show()
+  blinkt.set_pixel(0, 255, 255, 255)
+  blinkt.show()
+  ping = re.findall('Ping:\s(.*?)\s', response, re.MULTILINE)
+  download = re.findall('Download:\s(.*?)\s', response, re.MULTILINE)
+  upload = re.findall('Upload:\s(.*?)\s', response, re.MULTILINE)
+
+  ping[0] = ping[0].replace(',', '.')
+  download[0] = download[0].replace(',', '.')
+  upload[0] = upload[0].replace(',', '.')
